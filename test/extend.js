d63030c23bbe9500824a378ebc2f525a0b44965b
Ricardo Barros <ricardofbarros@hotmail.com>
2015-02-12 13:32:19 +0000

    add more tests, more to come in the near future

--- test/extend.js
+++ test/extend.js
@@ -4,7 +4,7 @@ var Mongules = require('./../index');
 
 // Start extend tests
 describe('Extend validator', function() {
-  it('Should add new validation', function(done_add) {
+  it('Should add new global validation', function(done_add) {
     Mongules.extend('hasNumber', function(str) {
       return /[0-9]/.test(str);
     });
@@ -12,17 +12,34 @@ describe('Extend validator', function() {
     done_add();
   });
 
-  it('Should validate with the new validation fn', function(done_validate) {
+  it('Should validate with the new global validation fn', function(done_validate) {
+    // global test
     expect(Mongules._validationMethods.hasNumber(1)).to.be(true);
-
     expect(Mongules._validationMethods.hasNumber('string')).to.be(false);
+
+    // create a new instance
+    var mongules = new Mongules();
+
+    // Test if global method was passed to instance
+    expect(mongules._validationMethods.hasNumber(1)).to.be(true);
+    expect(mongules._validationMethods.hasNumber('string')).to.be(false);
 
     done_validate();
   });
 
-  it('Should hook a message to new validation fn', function(done_hook) {
+  it('Should hook a message to new global validation fn', function(done_hook) {
+    // add message globally
     Mongules.hookMsg('hasNumber', 'extended-hook-msg');
+
+    // test globally
     expect(Mongules._msgHooks.hasNumber).to.be.equal('extended-hook-msg');
+
+    // create a new instance
+    var mongules = new Mongules();
+
+    // Test if global method was passed to instance
+    expect(mongules._msgHooks.hasNumber).to.be.equal('extended-hook-msg');
+
     done_hook();
   });
 });
